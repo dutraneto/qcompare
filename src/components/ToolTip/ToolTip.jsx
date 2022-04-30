f1a2f81af541c526c708dd4501ce81d001345d64
dutraneto <dutraneto@me.com>
2022-04-30 17:38:21 -0400

    Fix width

--- src/components/ToolTip/ToolTip.jsx
+++ src/components/ToolTip/ToolTip.jsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 
 function ToolTip({ iconUrl, title, description }) {
   return (
-    <div className="p-6 flex sm:items-end lg:items-center relative z-1 max-w-3xl gap-5">
+    <div className="p-6 flex sm:items-end lg:items-center relative z-1 w-2/3 gap-5">
       <div className="absolute -z-1 bg-yellow opacity-20 w-100 height-100 left-0 right-0 bottom-0 top-0"></div>
-      <Image src={iconUrl} width={170} height={171} />
+      <div className="lg:w-28 md:w-24">
+        <Image src={iconUrl} width={170} height={171} />
+      </div>
       <div className="">
         <h4 className="font-bold">{title}</h4>
         <p>{description}</p>
