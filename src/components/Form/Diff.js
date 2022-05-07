bb19066a1fa06c11d5f6474a830a3ab81c8c6328
dutraneto <dutraneto@me.com>
2022-05-06 22:55:46 -0400

    Fix loop issue on FormView

--- src/components/Form/Diff.js
+++ src/components/Form/Diff.js
@@ -1,42 +1,56 @@
-import diff from 'simple-text-diff'
+import * as diff from 'diff'
 import * as React from 'react'
 
-const Diff = ({ string1, string2 }) => {
-  const { before, after } = diff.diffPatch(string1, string2)
-  const mappedBefore = before
-    .split('\n')
-    .map((line) => {
-      return line === ''
-        ? `<div class='outputOriginal w-full h-5 mb-1'></div>`
-        : `<div class='outputOriginal mb-1'>${line}</div>`
-    })
-    .join('')
-  const mappedAfter = after
-    .split('\n')
-    .map((line) => {
-      return line === ''
-        ? `<div class='outputChanged w-full h-5  mb-1'></div>`
-        : `<div class='outputChanged mb-1'>${line}</div>`
-    })
-    .join('')
-  console.log('Before =>', mappedBefore)
-  console.log('After =>', mappedAfter)
+const styles = {
+  added: {
+    color: 'green',
+    bgColor: 'bg-green'
+  },
+  removed: {
+    color: 'pink',
+    bgColor: 'bg-pink'
+  }
+}
+
+const Diff = ({ string1, string2, mode }) => {
+  // const [groups, setGroups] = React.useState(diff.diffChars(string1, string2))
+  let groups = []
+  React.useEffect(() => {
+    window.diff = diff
+    if (mode === 'words') {
+      groups = diff.diffWords(string1, string2)
+    }
+    if (mode === 'characters') {
+      groups = diff.diffChars(string1, string2)
+    }
+  }, [])
+
+  const mappedNodes = groups.map((group, index) => {
+    const { value, added, removed } = group
+    let nodeStyles
+    if (added) nodeStyles = styles.added
+    if (removed) nodeStyles = styles.removed
+    return (
+      <span key={index} className={`lines w-full h-5 mb-1`} styles={nodeStyles}>
+        {value}
+      </span>
+    )
+  })
+  // const mappedNodes = groups.map((group) => {
+  //   const { value, added, remove } = group
+  //   let nodeStyles
+  //   if (added) nodeStyles = styles.added
+  //   if (removed) nodeStyles = styles.removed
+  //   return group === ''
+  //     ? `<div class=' lines w-full h-5 bg-palegreen mb-1'></div>`
+  //     : `<div class='lines'>${group}</div>`
+  // })
 
   return (
     <>
       <div className="flex gap-3 mb-12">
-        <div
-          className="flex-1 bg-gray-100 p-4"
-          dangerouslySetInnerHTML={{
-            __html: mappedBefore
-          }}
-        />
-        <div
-          className="flex-1 bg-gray-100 p-4"
-          dangerouslySetInnerHTML={{
-            __html: mappedAfter
-          }}
-        />
+        <div className="flex-1 bg-gray-100 p-4">{mappedNodes}</div>
+        <div className="flex-1 bg-gray-100 p-4">{mappedNodes}</div>
       </div>
     </>
   )
--- src/components/Form/FormView.jsx
+++ src/components/Form/FormView.jsx
@@ -5,29 +5,38 @@ import * as React from 'react'
 import confetti from 'canvas-confetti'
 
 function FormView() {
-  const stateOfButton = {
-    initialText: 'Compare Text',
-    bgColor: 'bg-blue'
+  function stateOfButton(args) {
+    setButtonState(args)
   }
   const [string1, setString1] = React.useState('')
   const [string2, setString2] = React.useState('')
-  const [buttonState, setButtonState] = React.useState(stateOfButton)
+  const [buttonState, setButtonState] = React.useState({
+    initialText: 'Compare Text',
+    bgColor: 'bg-blue'
+  })
+
   React.useEffect(() => {
     if (string1 === '' && string2 === '') {
-      setButtonState(stateOfButton)
+      stateOfButton({
+        initialText: 'Compare Text',
+        bgColor: 'bg-blue'
+      })
       return
     } else if (string1 === string2) {
-      setButtonState({
+      stateOfButton({
         initialText: 'Texts are Identical',
         bgColor: 'bg-green'
       })
+
       confetti()
     } else
-      setButtonState({
+      stateOfButton({
         initialText: 'Texts are Different',
         bgColor: 'bg-red'
       })
-  })
+
+    console.log('Aqui está renderizando')
+  }, [string1, string2])
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -36,11 +45,11 @@ function FormView() {
   }
 
   function diffView() {
-    return <Diff string1={string1} string2={string2} />
+    return <Diff string1={string1} string2={string2} mode={'characters'} />
   }
   return (
     <>
-      {string1 && string2 && diffView()}
+      {(string1 && diffView()) || (string2 && diffView)}
       <form className="form" onSubmit={handleSubmit}>
         <div className="flex gap-3 mb-4">
           <TextArea
