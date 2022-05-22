eadb43574f82fc9cd485871eb46f010a607d2ced
dutraneto <dutraneto@me.com>
2022-05-21 21:03:40 -0400

    Refactor input button

--- src/components/Form/FormView.jsx
+++ src/components/Form/FormView.jsx
@@ -1,14 +1,14 @@
+import * as React from 'react'
 import Diff from '../Diff/Diff'
 import TextArea from './TextArea'
 import Button from './Button'
-import * as React from 'react'
+import InputControls from './InputControls'
 import confetti from 'canvas-confetti'
 
 function FormView() {
   const [string1, setString1] = React.useState('')
   const [string2, setString2] = React.useState('')
   const [selectedEnabled, setSelectedEnabled] = React.useState('words')
-
   const [buttonState, setButtonState] = React.useState({
     initialText: 'Compare Text',
     bgColor: 'bg-blue'
@@ -26,14 +26,12 @@ function FormView() {
         initialText: 'Texts are Identical',
         bgColor: 'bg-green'
       })
-
       confetti()
     } else
       stateOfButton({
         initialText: 'Texts are Different',
         bgColor: 'bg-red'
       })
-    console.log(selectedEnabled)
   }, [string1, string2, selectedEnabled])
 
   function handleSubmit(event) {
@@ -77,39 +75,13 @@ function FormView() {
             bgColor={buttonState.bgColor}
           />
         </div>
-        <div className="flex items-center gap-5 absolute -right-44 hover:right-0 bottom-0 bg-gray-200 p-px transition-all">
-          <span className="animate-pulse">⬅</span>
-          <div className="flex items-baseline">
-            <input
-              className="cursor-pointer"
-              type="radio"
-              name="option"
-              id="words"
-              checked={selectedEnabled === 'words'}
-              onChange={() => setSelectedEnabled('words')}
-              value="words"
-              aria-label="words"
-            />
-            <label className="mx-2 my-0 cursor-pointer pd-0" htmlFor="words">
-              Words
-            </label>
-          </div>
-          <div className="flex items-baseline">
-            <input
-              className="cursor-pointer"
-              type="radio"
-              name="option"
-              id="chars"
-              checked={selectedEnabled === 'chars'}
-              onChange={() => setSelectedEnabled('chars')}
-              value="chars"
-              aria-label="chars"
-            />
-            <label className="mx-2 my-0 cursor-pointer pd-0" htmlFor="chars">
-              Chars
-            </label>
-          </div>
-        </div>
+        <ul className="flex items-center gap-5 absolute -right-44 hover:right-0 bottom-0 bg-gray-200 p-px transition-all list-none">
+          <li className="animate-pulse">⬅</li>
+          <InputControls
+            selectedEnabled={selectedEnabled}
+            setSelectedEnabled={setSelectedEnabled}
+          />
+        </ul>
       </form>
     </>
   )
