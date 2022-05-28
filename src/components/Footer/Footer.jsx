88373b2691766ff3fd07a9d9aeabca1dfcb120b0
dutraneto <dutraneto@me.com>
2022-05-28 13:03:02 -0400

    Issue Link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <footer className="self-center mb-6">
-      © 2022. Made with&nbsp;❤️&nbsp;&nbsp;by EA Team.
+      <div>
+        <div className="text-center">
+          © 2022. Made with&nbsp;❤️&nbsp;&nbsp;by EA Team.
+        </div>
+        <div className="text-center">
+          Found an issue?{' '}
+          <a className="underline text-blue" href="mailto:jdutra@quarry.com">
+            Email Us
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
