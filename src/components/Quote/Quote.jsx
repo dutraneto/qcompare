672b48b0fa50a62d9fcb341fc735cc180610f24b
dutraneto <dutraneto@me.com>
2023-01-21 17:07:23 -0500

    Add state to change logo in dark/light mode

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Quote() {
   const url = 'https://api.quotable.io/random'
   const { content, author, isLoading, isError } = useApi(url)
-  // console.log(data)
+
   if (isLoading) return <p>loading ...</p>
   if (isError) return <p>Error fetching API</p>
   return (
