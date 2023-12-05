9a57e7964d35a971e7bcb1de580854b25a4c893c
dutraneto <dutraneto@me.com>
2023-12-05 16:15:41 -0500

    Add Christmas theme

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,22 @@
 import ToolTip from 'components/ToolTip/ToolTip'
 import Quote from 'components/Quote/Quote'
 import FormView from '../Form/FormView'
+import Canvas from 'components/Canvas/Canvas'
 
 function Main(props) {
   return (
-    <main className="bg-white dark:bg-slate-900 p-6">
-      <section className="flex flex-col sm:flex-row gap-5  mb-5">
-        <ToolTip {...props} />
-        <Quote />
-      </section>
-      <section className="py-6 px-3 mb-5 shadow-sm rounded">
-        <FormView />
-      </section>
-    </main>
+    <>
+      <Canvas />
+      <main className="bg-white dark:bg-slate-900 p-6">
+        <section className="flex flex-col sm:flex-row gap-5  mb-5">
+          <ToolTip {...props} />
+          <Quote />
+        </section>
+        <section className="py-6 px-3 mb-5 shadow-sm rounded">
+          <FormView />
+        </section>
+      </main>
+    </>
   )
 }
 
--- src/components/ToolTip/ToolTip.jsx
+++ src/components/ToolTip/ToolTip.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
-function ToolTip({ iconUrl, title, description }) {
+function ToolTip({ iconChristmas, title, description }) {
   return (
     <div className="p-6 flex sm:items-end lg:items-center relative z-1 w-full sm:w-3/5 gap-5 rounded">
       <div className="absolute -z-1 bg-yellow dark:bg-[#1E293B] opacity-30 dark:opacity-100 w-100 height-100 left-0 right-0 bottom-0 top-0 rounded"></div>
       <div className="lg:w-28 md:w-24">
-        <Image src={iconUrl} width={170} height={171} />
-        {/* <Image src={iconChristmas} width={170} height={171} /> */}
+        {/* <Image src={iconUrl} width={170} height={171} /> */}
+        <Image src={iconChristmas} width={170} height={171} />
       </div>
       <div className="z-50" style={{ color: 'black' }}>
         <h4 className="text-black dark:text-white">{title}</h4>
