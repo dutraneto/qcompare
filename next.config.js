fac09a3f4e2eb015b0ec1422aab21fe83619fa0a
dutraneto <dutraneto@me.com>
2024-04-10 14:57:22 -0400

    Update next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /* eslint-disable no-undef */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 // eslint-disable-next-line no-undef
 module.exports = withPWA({
-  reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com', 'www.quarry.com', 'openweathermap.org']
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd,
-    runtimeCaching
   }
 })
