646dc64fdd2fbe24601073cc781efcfcaa6840b5
dutraneto <dutraneto@me.com>
2024-04-11 06:54:50 -0400

    Change Icon app and improve description

--- src/components/ToolTip/ToolTip.jsx
+++ src/components/ToolTip/ToolTip.jsx
@@ -5,11 +5,11 @@ function ToolTip({ iconUrl, title, description }) {
     <div className="p-6 flex sm:items-end lg:items-center relative z-1 w-full sm:w-3/5 gap-5 rounded">
       <div className="absolute -z-1 bg-yellow dark:bg-[#1E293B] opacity-30 dark:opacity-100 w-100 height-100 left-0 right-0 bottom-0 top-0 rounded"></div>
       <div className="lg:w-28 md:w-24">
-        <Image src={iconUrl} width={170} height={171} />
+        <Image src={iconUrl} width={200} height={200} />
         {/* <Image src={iconChristmas} width={170} height={171} /> */}
       </div>
       <div className="z-50" style={{ color: 'black' }}>
-        <h4 className="text-black dark:text-white">{title}</h4>
+        <h4 className="text-xl font-bold text-black dark:text-white">{title}</h4>
         <p
           className="text-black dark:text-white"
           dangerouslySetInnerHTML={{
