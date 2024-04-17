1ae0a0750bed57fbe6e0ab3b0fd1d9c800d5f1b5
dutraneto <dutraneto@me.com>
2024-04-16 21:33:40 -0400

    Improve PWA config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-undef */
-const withPWA = require("@imbios/next-pwa")({
-  dest: "public",
-});
-const runtimeCaching = require('@imbios/next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('@imbios/next-pwa')({
+  dest: 'public'
+})
 
 // eslint-disable-next-line no-undef
 module.exports = withPWA({
@@ -12,8 +10,8 @@ module.exports = withPWA({
     domains: ['res.cloudinary.com', 'www.quarry.com', 'openweathermap.org']
   },
   pwa: {
-    dest: 'public',
-    disable: !isProd,
-    runtimeCaching
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
   }
 })
